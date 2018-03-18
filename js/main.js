e8dea0a7d0c7be3556e53779f42e21ab763bb647
Guchiechan <36108529+Guchiechan@users.noreply.github.com>
2018-03-18 19:13:03 +0000

    Got filter working, need to put in resources when list is ready

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ $('#b1').click(function() {
 	  });
 
 	});
-	
+
 	// When the user scrolls down 20px from the top of the document, show the button
 	window.onscroll = function() {scrollFunction()};
 
@@ -58,3 +58,40 @@ $('#b1').click(function() {
 	    document.body.scrollTop = 0; // For Safari
 	    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 	}
+
+
+
+	/* Storing selection values */
+$('button[name=Option1]').click(function() {
+  $('button[name=Option1]').removeAttr('id');
+  $(this).attr('id', 'selected');
+});
+
+$('button[name=Option2]').click(function() {
+  $('button[name=Option2]').removeAttr('id');
+  $(this).attr('id', 'selected');
+});
+
+$('button[name=Option3]').click(function() {
+  $('button[name=Option3]').removeAttr('id');
+  $(this).attr('id', 'selected');
+});
+
+/* Filtering */
+$('#submit').click(function() {
+
+  var val1 = $('[name=Option1][id=selected]').val();
+  var val2 = $('[name=Option2][id=selected]').val();
+  var val3 = $('[name=Option3][id=selected]').val();
+  $('tr').each(function() {
+    var src = $(this).find('.Time').text();
+    var typ = $(this).find('.Location').text();
+    var knd = $(this).find('.Type').text();
+
+    if ((src == val1 || val1 == 'A') && (typ == val2 || val2 == 'A') && (knd == val3 || val3 == 'A')) {
+      $(this).show();
+    } else {
+      $(this).hide();
+    }
+  });
+});
